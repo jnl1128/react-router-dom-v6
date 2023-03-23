c27053f37ffd4e3af1b4860f7f67ca68510c7cdd
narin <nalynnshinhwa1128@gmail.com>
2023-03-23 20:02:33 +0900

    outlet과 상대경로

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Root from "./Root";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
 import User from "./screens/users/User";
+import Followers from "./screens/users/Followers";
 
 const router = createBrowserRouter([
     {
@@ -24,9 +25,12 @@ const router = createBrowserRouter([
             {
                 path: "users/:userId",
                 element: <User />,
-                // :userId를 children으로 계층화하지 않은 이유
-                // users 만을 위한 컴포넌트를 따로 만들어야 하는데
-                // 그걸 의도한 것이 아니기 때문.
+                children: [
+                    {
+                        path: "followers",
+                        element: <Followers />,
+                    },
+                ],
             },
         ],
         errorElement: <NotFound />,

d159fd9e3b9fb780dbdc936fb74335c3267170b2
narin <nalynnshinhwa1128@gmail.com>
2023-03-23 18:07:50 +0900

    컴포넌트 에러, 라우팅 경로 에러시 errorElement 처리

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,23 +3,9 @@ import Header from "./components/Header";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import Root from "./Root";
-// function Router() {
-//     return (
-//         <BrowserRouter>
-//             <Header />
-//             <Routes>
-//                 <Route path="/" element={<Home />}></Route>
-//                 <Route path="/about" element={<About />}></Route>
-//             </Routes>
-//         </BrowserRouter>
-//     );
-// }
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
-// Router를 array 형식으로 표현할 수 있게 해줌.
-// url: /about이면
-// Root가 먼저 render하고
-// About이 render함.
-// Outlet을 내가 render하고자 하는 route로 바꿔서 render함.
 const router = createBrowserRouter([
     {
         path: "/",
@@ -28,12 +14,14 @@ const router = createBrowserRouter([
             {
                 path: "",
                 element: <Home />,
+                errorElement: <ErrorComponent />,
             },
             {
                 path: "about",
                 element: <About />,
             },
         ],
+        errorElement: <NotFound />,
     },
 ]);
 export default router;
